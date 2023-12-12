4c6827553ad99c9ceaf698dccd1d5156cc82fe84
Khairul Kabir <sshakil.kabir60@gmail.com>
2023-12-12 13:24:03 +0600

    Service and Controller Updated

--- Patient.Service/Repository/IRepository/IPatientRepository.cs
+++ Patient.Service/Repository/IRepository/IPatientRepository.cs
@@ -5,5 +5,7 @@ namespace Patient.Service.Repository.IRepository
     public interface IPatientRepository
     {
         public Task<List<PatientViewModel>> GetAllPatients();
+
+        Task<PatientViewModel> SavePatient(PatientViewModel model);
     }
 }
--- Patient.Service/Repository/PatientRepository.cs
+++ Patient.Service/Repository/PatientRepository.cs
@@ -20,5 +20,42 @@ namespace Patient.Service.Repository
                 return patients.ToList();
             }
         }
+
+        public async Task<PatientViewModel> SavePatient(PatientViewModel patientViewModel)
+        {
+            // Assuming you have a Patients table in your database
+            const string insertQuery = @"
+            INSERT INTO Patients (FirstName, LastName, Age, Gender, Image, Phone)
+            VALUES (@FirstName, @LastName, @Age, @Gender, @Image, @Phone);
+            SELECT LAST_INSERT_ID();";
+
+            using (var connection = _dbContext.CreateConnection())
+            {
+                connection.Open();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        // Execute the insert query and retrieve the last inserted ID
+                        int patientId = await connection.QuerySingleAsync<int>(insertQuery, patientViewModel, transaction);
+
+                        // Set the ID of the view model with the newly inserted ID
+                        patientViewModel.Id = patientId;
+
+                        // Commit the transaction
+                        transaction.Commit();
+
+                        return patientViewModel;
+                    }
+                    catch (Exception)
+                    {
+                        // Handle exceptions, log, or rollback the transaction
+                        transaction.Rollback();
+                        throw; // You may want to handle or log the exception according to your needs
+                    }
+                }
+            }
+        }
     }
 }
